73ff684b0b6e6ec121d1ca0b5169a62ccab389f1
René Globig <rene@globig.org>
2021-01-24 01:41:37 +0100

    added more to the test

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Numerics;
+using System.Threading;
 
 namespace qt_benchmark
 {
@@ -20,7 +21,7 @@ namespace qt_benchmark
 
 			test.Start();
 
-			var ticks = 3000;
+			var ticks = 100;
 
 			var total = 0L;
 			var average = 0L;
@@ -41,8 +42,10 @@ namespace qt_benchmark
 				if (current > highest) highest = current;
 				if (current < lowest) lowest = current;
 
+				Console.Clear();
 				test.Draw();
 				watch.Reset();
+				Thread.Sleep(10);
 			}
 
 			Console.WriteLine($"Average: {average}ms / Highest: {highest}ms / Lowest: {lowest}ms");
@@ -53,11 +56,8 @@ namespace qt_benchmark
 	class Basic : IQuadTreeService
 	{
 		public void Initialize() { }
-
 		public void Query(Vector2 position, float radius, Dictionary<int, Agent> allAgents, HashSet<Agent> buffer)
 		{
-			buffer.Clear();
-
 			for (int i = 0; i < allAgents.Count; i++)
 			{
 				var agent = allAgents[i];
@@ -68,7 +68,6 @@ namespace qt_benchmark
 				}
 			}
 		}
-
 		public void Update() { }
 	}
 
@@ -83,14 +82,14 @@ namespace qt_benchmark
 	{
 		public int id;
 		public float radius;
-
 		public Vector2 position;
-
 		public Vector2 move;
-
 		public float speed;
-
 		public void Move() => position += move * speed;
+		public override bool Equals(object obj) => obj is Agent agent && id == agent.id;
+		public override int GetHashCode() => id.GetHashCode();
+		public static bool operator ==(Agent a, Agent b) => a.id == b.id;
+		public static bool operator !=(Agent a, Agent b) => !(a == b);
 	}
 
 	class Map
@@ -101,14 +100,19 @@ namespace qt_benchmark
 
 	class Test
 	{
-		private readonly Dictionary<int, Agent> agents = new Dictionary<int, Agent>();
+		private readonly Dictionary<int, Agent> agents;
 		private readonly IQuadTreeService qt;
 		private readonly Map map;
 		private readonly Random random;
+		private readonly HashSet<Agent> buffer;
+		private readonly Vector2 Up = new Vector2(0, 1);
+		private readonly Vector2 Right = new Vector2(1, 0);
 
 		public Test(int seed, IQuadTreeService qt, Map map, int agentAmount, float agentRadius, float agentSpeed)
 		{
 			random = new Random(seed);
+			agents = new Dictionary<int, Agent>(agentAmount);
+			buffer = new HashSet<Agent>(32);
 			this.qt = qt;
 			this.map = map;
 
@@ -138,15 +142,74 @@ namespace qt_benchmark
 
 			qt.Update();
 
+			buffer.Clear();
+
 			foreach (var agent in agents)
 			{
+				qt.Query(agent.Value.position, agent.Value.radius, agents, buffer);
 
+				foreach (var other in buffer)
+				{
+					if (agent.Value != other)
+					{
+						agent.Value.move = Vector2.Normalize(new Vector2(random.Next(), random.Next()));
+					}
+				}
+			}
+
+			foreach (var agent in agents)
+			{
+				var position = agent.Value.position;
+
+				if (position.X <= 0)
+				{
+					position.X = 0;
+					agent.Value.move = Vector2.Reflect(agent.Value.move, Up);
+				}
+				if (position.Y <= 0)
+				{
+					position.Y = 0;
+					agent.Value.move = Vector2.Reflect(agent.Value.move, Right);
+				}
+				if (position.X >= map.sizeX - 1)
+				{
+					position.X = map.sizeX - 1;
+					agent.Value.move = Vector2.Reflect(agent.Value.move, Up);
+				}
+				if (position.Y >= map.sizeY - 1)
+				{
+					position.Y = map.sizeY - 1;
+					agent.Value.move = Vector2.Reflect(agent.Value.move, Right);
+				}
+
+				agent.Value.position = position;
 			}
 		}
 
 		public void Draw()
 		{
+			for (int row = 0; row < map.sizeY; row++)
+			{
+				var line = string.Empty;
 
+				for (int i = 0; i < map.sizeX * 2; i++)
+				{
+					line += ' ';
+				}
+
+				foreach (var agent in agents)
+				{
+					int y = Convert.ToInt32(Math.Round(agent.Value.position.Y));
+					if (row == y)
+					{
+						int x = Convert.ToInt32(Math.Round(agent.Value.position.X));
+						var chars = line.ToCharArray();
+						chars[x * 2] = 'o';
+						line = new string(chars);
+					}
+				}
+				Console.WriteLine(line);
+			}
 		}
 	}
 }
