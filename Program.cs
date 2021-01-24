58cdfa1eadde963fd5fcf429796486c29eb2675c
René Globig <rene@globig.org>
2021-01-24 11:59:10 +0100

    fixed some bug

--- Program.cs
+++ Program.cs
@@ -107,6 +107,8 @@ namespace qt_benchmark
 		private readonly HashSet<Agent> buffer;
 		private readonly Vector2 Up = new Vector2(0, 1);
 		private readonly Vector2 Right = new Vector2(1, 0);
+		private readonly Vector2 Left = new Vector2(-1, 0);
+		private readonly Vector2 Down = new Vector2(0, -1);
 
 		public Test(int seed, IQuadTreeService qt, Map map, int agentAmount, float agentRadius, float agentSpeed)
 		{
@@ -123,11 +125,19 @@ namespace qt_benchmark
 					id = i,
 					radius = agentRadius,
 					position = new Vector2(random.Next(0, map.sizeX), random.Next(0, map.sizeY)),
-					move = Vector2.Normalize(new Vector2(random.Next(), random.Next())),
+					move = Vector2.Normalize(RandomDirection()),
 					speed = agentSpeed
 				});
 			}
 		}
+
+		Vector2 RandomDirection()
+		{
+			var x = random.Next(-100, 101);
+			var y = random.Next(-100, 101);
+			return new Vector2(x, y);
+		}
+
 		public void Start()
 		{
 			qt.Initialize();
@@ -152,7 +162,7 @@ namespace qt_benchmark
 				{
 					if (agent.Value != other)
 					{
-						agent.Value.move = Vector2.Normalize(new Vector2(random.Next(), random.Next()));
+						agent.Value.move = -agent.Value.move;
 					}
 				}
 			}
@@ -164,22 +174,22 @@ namespace qt_benchmark
 				if (position.X <= 0)
 				{
 					position.X = 0;
-					agent.Value.move = Vector2.Reflect(agent.Value.move, Up);
+					agent.Value.move = Vector2.Reflect(agent.Value.move, Right);
 				}
 				if (position.Y <= 0)
 				{
 					position.Y = 0;
-					agent.Value.move = Vector2.Reflect(agent.Value.move, Right);
+					agent.Value.move = Vector2.Reflect(agent.Value.move, Up);
 				}
 				if (position.X >= map.sizeX - 1)
 				{
 					position.X = map.sizeX - 1;
-					agent.Value.move = Vector2.Reflect(agent.Value.move, Up);
+					agent.Value.move = Vector2.Reflect(agent.Value.move, Left);
 				}
 				if (position.Y >= map.sizeY - 1)
 				{
 					position.Y = map.sizeY - 1;
-					agent.Value.move = Vector2.Reflect(agent.Value.move, Right);
+					agent.Value.move = Vector2.Reflect(agent.Value.move, Down);
 				}
 
 				agent.Value.position = position;
